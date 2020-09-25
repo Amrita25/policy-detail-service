c34c763b755b641529bc5b1c37ef6cd0c1298726
Amrita25 <amrita.majumdar25@gmail.com>
2020-09-25 23:37:10 +0530

    first commit

---
+++ src/main/java/io/policydetailservice/policydetailservice/PolicyDetailServiceApplication.java
@@ -0,0 +1,13 @@
+package io.policydetailservice.policydetailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyDetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyDetailServiceApplication.class, args);
+	}
+
+}
